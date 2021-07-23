5f1f9194b2a82d6016306b7e286225251f24bb5d
Chris Leung <cleungdev@gmail.com>
2021-07-23 17:52:11 -0400

    set up axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  // running the API call once on mount
+  useEffect( () => {
+    const apiKey = '4799fee60bb1f4fcd88ccbf374cf3598'
+  
+    axios({
+      url: 'https://api.themoviedb.org/3',
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        api_key: apiKey,
+        format: 'json'
+      }
+    }).then((response) => {
+      console.log(response);
+    })
+  
+  })
   return (
     <div className="App">
       <h1>Oh Hi</h1>
+
+
     </div>
   );
 }
