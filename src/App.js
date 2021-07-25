dabb545766b6629cdd3a55ac43c9ac72a050ea86
Chris Leung <cleungdev@gmail.com>
2021-07-24 22:02:59 -0400

    created component to retrieve and render trending movies

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import './App.css';
+import DisplayMovie from './DisplayMovie';
+import './DisplayMovie.css'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
+
+
 function App() {
+  // initialize state
+  const [ movieArr, setMovieArr ] = useState([])
+
+  // find movies
+
   // running the API call once on mount
   useEffect( () => {
     const apiKey = '4799fee60bb1f4fcd88ccbf374cf3598'
   
     axios({
-      url: 'https://api.themoviedb.org/3',
+      url: 'https://api.themoviedb.org/3/trending/all/week',
       method: 'GET',
       dataResponse: 'json',
       params: {
         api_key: apiKey,
-        format: 'json'
+        format: 'json',
       }
-    }).then((response) => {
-      console.log(response);
+    }).then((res) => {
+      setMovieArr(res.data.results);
     })
-  
-  })
+
+  },[])
+
+  // console.log(movieArr);
+
   return (
     <div className="App">
-      <h1>Oh Hi</h1>
+      <h1>Trending</h1>
 
-
+      <div>
+        {
+          movieArr.map( movie => {
+            return (
+              <DisplayMovie key={movie.id} movieObj={movie} />
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
