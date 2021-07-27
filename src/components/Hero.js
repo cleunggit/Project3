19289a4b0b39cbe42b5732a62c1170353a8f6ada
Chris Leung <cleungdev@gmail.com>
2021-07-26 21:52:33 -0400

    added hero bg and text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,22 @@
+import { FaPlay, FaInfo } from 'react-icons/fa';
 import heroImage from '../assets/hero-image.png';
+import titleImage from '../assets/hero-title.png';
 
 const Banner = () => {
 
   return (
     <section className="hero">
-      <div className="hero__container">
         <div className="hero__image">
-          {/* <h2>Hello</h2> */}
-          <img src={heroImage} alt="Image of The Goonies movie poster" />
+            {/* <img src={heroImage} alt="Image of The Goonies movie poster" /> */}
+        </div>
+      <div className="hero__container">
+        <div className="hero__text">
+          <img src={titleImage} alt="Goonies movie title" />
+          <p>A young teenager named Mikey Walsh finds an old treasure map in his father's attic. Hoping to save their homes from demolition, Mikey and his friends Data Wang, Chunk Cohen, and Mouth Devereaux run off on a big quest to find the secret stash of Pirate One-Eyed Willie.</p>
+          <div className="hero__buttons">
+            <button className="hero__buttons-play"><FaPlay className="hero__buttons-icon" />Play</button>
+            <button className="hero__buttons-info"><FaInfo className="hero__buttons-icon" />More Info</button>
+          </div>
         </div>
       </div>
     </section>
