e00c5fa50d7454037203fb9a9f2ae0b4cbaa1514
Chris Leung <cleungdev@gmail.com>
2021-07-28 18:14:06 -0400

    removed apikey

--- src/App.js
+++ src/App.js
@@ -15,11 +15,10 @@ function App() {
 
   // running the API call once on mount
   useEffect(() => {
-    const apiKey = "4799fee60bb1f4fcd88ccbf374cf3598";
+    const apiKey = "";
 
     axios({
       url: "https://api.themoviedb.org/3/trending/all/week",
-      // url: 'https://api.themoviedb.org/3/search/movie',
       method: "GET",
       dataResponse: "json",
       params: {
